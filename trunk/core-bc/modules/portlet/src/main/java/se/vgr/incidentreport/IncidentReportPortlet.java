e67aa030d89bb530a7d86a72b6c69aa82e376ed4
sofia.jonsson@callistaenterprise.se <sofia.jonsson@callistaenterprise.se@fe4735b6-2b13-11de-ba44-d58fe8a9ce33>
2009-04-24 09:57:15 +0000

    minimal portlet with iframe

---
+++ trunk/core-bc/modules/portlet/src/main/java/se/vgr/incidentreport/IncidentReportPortlet.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2009 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgr.incidentreport;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+
+
+public class IncidentReportPortlet extends GenericPortlet {
+	
+	public void init(PortletConfig config) throws PortletException {
+		super.init(config);
+	}
+
+	public void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+
+		response.setContentType("text/html");
+		PortletRequestDispatcher dispatcher = getPortletContext().getRequestDispatcher(
+			"/jsp/index.jsp");
+		
+		dispatcher.include(request, response);
+	}
+
+	public void processAction(ActionRequest request, ActionResponse response)
+			throws PortletException, IOException {
+	
+		
+	}
+
+}
---
+++ trunk/core-bc/modules/portlet/src/main/webapp/jsp/index.jsp
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<%--
+
+    Copyright 2009 Västra Götalandsregionen
+
+      This library is free software; you can redistribute it and/or modify
+      it under the terms of version 2.1 of the GNU Lesser General Public
+      License as published by the Free Software Foundation.
+
+      This library is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU Lesser General Public License for more details.
+
+      You should have received a copy of the GNU Lesser General Public
+      License along with this library; if not, write to the
+      Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+      Boston, MA 02111-1307  USA
+
+--%>
+<html xmlns:xf="http://www.w3.org/2002/xforms"
+    xmlns="http://www.w3.org/1999/xhtml"
+    xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:ev="http://www.w3.org/2001/xml-events"
+    xml:lang="sv" lang="sv">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />	
+ 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css"/>
+	<title>Tyck till</title>
+	
+</head>
+<body> 
+<iframe 
+	src="http://localhost:8082/tyck-till-core-bc-module-web/xforms-jsp/index.jsp" 
+	width="100%" 
+	height="100%"
+	frameborder="0">
+  <p>Din webbläsare stödjer ej IFrames.</p>
+</iframe>
+
+</body> 
+
+</html>

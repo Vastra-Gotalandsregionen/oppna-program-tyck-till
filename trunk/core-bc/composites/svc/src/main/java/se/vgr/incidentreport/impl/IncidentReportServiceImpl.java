daea977a4f081a9974af920dca7ee82e06d18f00
sofia.jonsson@callistaenterprise.se <sofia.jonsson@callistaenterprise.se@fe4735b6-2b13-11de-ba44-d58fe8a9ce33>
2009-05-15 10:45:05 +0000

    added spring support

---
+++ trunk/core-bc/composites/svc/src/main/java/se/vgr/incidentreport/impl/IncidentReportServiceImpl.java
@@ -0,0 +1,10 @@
+package se.vgr.incidentreport.impl;
+
+import org.springframework.stereotype.Service;
+
+import se.vgr.incidentreport.IncidentReportService;
+
+@Service("incidentReportService")
+public class IncidentReportServiceImpl implements IncidentReportService {
+
+}

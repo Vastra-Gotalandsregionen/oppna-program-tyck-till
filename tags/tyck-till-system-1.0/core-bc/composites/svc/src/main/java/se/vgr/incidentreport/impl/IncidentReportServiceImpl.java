eecdccfeaec1530a76939d8c8ad2fcd4c9b1cf80
arkitekthatt <arkitekthatt@fe4735b6-2b13-11de-ba44-d58fe8a9ce33>
2009-11-12 13:19:59 +0000

    [maven-scm] copy for tag tyck-till-system-1.0

---
+++ tags/tyck-till-system-1.0/core-bc/composites/svc/src/main/java/se/vgr/incidentreport/impl/IncidentReportServiceImpl.java
@@ -0,0 +1,231 @@
+/**
+ * Copyright 2009 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgr.incidentreport.impl;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import se.vgr.incidentreport.IncidentReport;
+import se.vgr.incidentreport.IncidentReportService;
+import se.vgr.incidentreport.Screenshot;
+import se.vgr.incidentreport.pivotaltracker.PTStory;
+import se.vgr.incidentreport.pivotaltracker.PivotalTrackerService;
+import se.vgr.usdservice.USDService;
+import se.vgr.util.EMailClient;
+
+@Service("incidentReportService")
+public class IncidentReportServiceImpl implements IncidentReportService {
+
+    @Autowired
+    @Qualifier("pivotalTrackerMappings")
+    private Properties pivotalTrackerMappings;
+
+    /**
+     * Reciever of messages when the regular report recievers can´t be reached.
+     */
+    private static final String INCIDENT_REPORT_SERVICE_ADMIN_EMAIL = "ulcan@wmdata.com";
+
+    /**
+     * Email reply address.
+     */
+    private static final String INCIDENT_REPORT_SERVICE_NOREPLY = "IncidentReportService-noreply@vgregion.se";
+
+    /**
+     * Email subject when something is not working as expected in Tyck till
+     */
+    private static final String INCIDENT_REPORT_ERROR_EMAIL_SUBJECT = "*ERROR in IncidentReportService* ";
+
+    /**
+     * Email subject
+     */
+    private static final String INCIDENT_REPORT_EMAIL_SUBJECT = "IncidentReportService message";
+
+    final Logger logger = LoggerFactory.getLogger(IncidentReportServiceImpl.class);
+
+    @Autowired
+    @Qualifier("usdService")
+    private USDService usdService;
+
+    @Autowired
+    private PivotalTrackerService pivotalTrackerClient;
+
+    public int reportIncident(IncidentReport ir) {
+
+        int result = 0;
+
+        try {
+
+            String method = ir.getReportMethod();
+            // String appName = ir.getApplicationName().replaceAll(" ", "_");
+            // String PTId = pivotalTrackerClient.getPivotalTrackerMappings().getProperty(appName);
+            // // Pivotaltracker is used if mapping exists
+            // if (PTId != null) {
+            // try {
+            // createUserStory(ir);
+            // }
+            // catch (Exception e) {
+            // log.warn(e);
+            // sendReportByEmail(ir, INCIDENT_REPORT_ERROR_EMAIL_SUBJECT + ":" + e.getMessage());
+            // }
+            //
+            // }
+            // else
+            if ("usd".equalsIgnoreCase(method)) {
+                try {
+                    Properties parameters = mapToRequestParameters(ir);
+                    List<Screenshot> ss = ir.getScreenShots();
+                    List<File> files = new ArrayList<File>();
+                    for (Screenshot s : ss) {
+                        files.add(new File(s.getPath()));
+                    }
+
+                    usdService.createRequest(parameters, ir.getUserId(), files);
+                }
+                catch (Exception e) {
+                    logger.warn("USD service could not be reached, trying email instead.", e);
+                    sendReportByEmail(ir, INCIDENT_REPORT_ERROR_EMAIL_SUBJECT + ":" + e.getMessage());
+                }
+            }
+            else if ("pivotaltracker".equalsIgnoreCase(method)) {
+                try {
+                    createUserStory(ir);
+                }
+                catch (Exception e) {
+                    logger.warn("Pivotal tracker could not be reached, trying email instead.");
+                    sendReportByEmail(ir, INCIDENT_REPORT_ERROR_EMAIL_SUBJECT + ":" + e.getMessage());
+                }
+            }
+            else {
+                sendReportByEmail(ir, INCIDENT_REPORT_EMAIL_SUBJECT);
+            }
+        }
+        catch (Exception e) {
+            logger.error("Incident report could not be sent:", e);
+            result = -1;
+        }
+        return result;
+    }
+
+    private void createUserStory(IncidentReport ir) {
+        String applicationName = ir.getApplicationName().replaceAll(" ", "_");
+        String projectId = lookupProjectId(applicationName);
+        PTStory story = new PTStory();
+        story.setName(applicationName + ": IncidentReportService message");
+        story.setType("bug");
+        story.setProjectId(projectId);
+        // story.setRequestedBy(TYCK_TILL_PT_USER);
+        story.setDescription(ir.toString());
+        // String url = addStoryForProject(projectId, story);
+        String url = pivotalTrackerClient.createuserStory(story);
+
+        // attachments cannot be sent to Pivotal Tracker and thus have to be emailed
+        List<Screenshot> attachments = ir.getScreenShots();
+        if (url != null && attachments != null && attachments.size() > 0) {
+            if (ir.getReportEmail() != null && ir.getReportEmail().length() > 0) {
+                try {
+                    ir.setLink("User story: <a href='" + url + "'>" + url + "</a></b>");
+
+                    sendReportByEmail(ir, "Bifogade filer som skall till pivotal tracker.");
+                }
+                catch (MessagingException e) {
+
+                }
+            }
+        }
+
+    }
+
+    private void sendReportByEmail(IncidentReport ir, String subject) throws MessagingException {
+        String reportEmail = ir.getReportEmail();
+
+        if (reportEmail != null && !"".equals(reportEmail)) {
+            String body = "";
+            String[] reportEmailArray = { reportEmail };
+            body += ir.toString().replaceAll(IncidentReport.NEWLINE, "</br>");
+            try {
+                new EMailClient().postMail(reportEmailArray, ir.getApplicationName() + ":" + subject, "" + body,
+                        INCIDENT_REPORT_SERVICE_NOREPLY, ir.getScreenShots());
+            }
+            catch (Throwable e1) {
+
+                logger.warn("Email submission failed:", e1);
+                String[] emailTo = { INCIDENT_REPORT_SERVICE_ADMIN_EMAIL };
+
+                new EMailClient().postMail(emailTo, ir.getApplicationName() + ":"
+                        + INCIDENT_REPORT_ERROR_EMAIL_SUBJECT, "" + e1.getMessage() + "\n" + body,
+                        INCIDENT_REPORT_SERVICE_NOREPLY, ir.getScreenShots());
+
+            }
+
+        }
+    }
+
+    private Properties mapToRequestParameters(IncidentReport ir) {
+        Properties p = new Properties();
+        p.setProperty("affected_resource", "nr:BF5880E3AF1C8542B2546B93922C25A7");
+        p.setProperty("category", "pcat:400023");
+        // map to group using application name?
+        String appName = ir.getApplicationName().trim().replaceAll(" ", "_");
+        System.out.println("getting group for appName=" + appName);
+        String groupHandle = usdService.getUSDGroupHandleForApplicationName(appName);
+        // p.setProperty("group", "cnt:A455761E38B4B8488B5F0999BE5A4637");
+        p.setProperty("group", groupHandle);
+        p.setProperty("impact", "imp:1603");
+        p.setProperty("priority", "pri:500");
+        p.setProperty("type", "crt:182");
+        p.setProperty("urgency", "urg:1100");
+        p.setProperty("z_location", "loc:67F817D782E87B45A8298FC5512B6A9C");
+        p.setProperty("z_organization", "org:5527E3F8D19F49409036F162493C7DD0");
+        p.setProperty("z_telefon_nr", ir.getPhoneNumber() != null ? ir.getPhoneNumber() : "N/A");
+
+        StringBuffer descBuf = new StringBuffer();
+
+        descBuf.append(ir.toString() + "\n");
+        p.setProperty("description", descBuf.toString());
+
+        return p;
+    }
+
+    private String lookupProjectId(String applicationName) {
+        String result = pivotalTrackerMappings.getProperty(applicationName);
+        if (result == null || result.length() == 0) {
+            result = applicationName;
+        }
+        return result;
+
+    }
+
+    public Properties getPivotalTrackerMappings() {
+        return pivotalTrackerMappings;
+    }
+
+    public void setPivotalTrackerMappings(Properties pivotalTrackerMappings) {
+        this.pivotalTrackerMappings = pivotalTrackerMappings;
+    }
+
+}
---
+++ tags/tyck-till-system-1.0/core-bc/modules/web/src/main/java/se/vgr/incidentreport/IncidentReportReader.java
@@ -0,0 +1,192 @@
+/**
+ * Copyright 2009 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgr.incidentreport;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import se.vgr.util.UserAgentUtils;
+
+@Consumes("application/xml")
+@Provider
+public class IncidentReportReader implements MessageBodyReader<IncidentReport> {
+
+    private static final Logger logger = LoggerFactory.getLogger(IncidentReportReader.class);
+    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    public IncidentReport readFrom(Class<IncidentReport> c, Type genericType, Annotation[] annotations,
+            javax.ws.rs.core.MediaType arg3, MultivaluedMap<String, String> arg4, InputStream is)
+            throws IOException, WebApplicationException {
+        try {
+            return parseIncidentReport(is);
+        }
+        catch (Exception e) {
+            throw new WebApplicationException(e);
+        }
+
+    }
+
+    protected IncidentReport parseIncidentReport(InputStream is) {
+
+        IncidentReport ir = new IncidentReport();
+        DocumentBuilder builder;
+        Document doc;
+        try {
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(is);
+            // System.out.println(doc.getTextContent());
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error creating xml", e);
+        }
+
+        ir.setReportType(parseString(doc, "reportType"));
+        String[] errorTypes = parseString(doc, "errorType").split(" ");
+        for (String errorType : errorTypes) {
+            ir.addErrorType(errorType);
+        }
+
+        ir.setDescription(parseString(doc, "description"));
+        ir.setDefaultErrorMessage(parseString(doc, "defaultErrorMessage"));
+        ir.setBrowser(UserAgentUtils.getBrowser(parseString(doc, "browser")));
+        ir.setOs(UserAgentUtils.getOS(parseString(doc, "browser")));
+        ir.setJavascript(parseString(doc, "javascript-enabled"));
+        String timestamp = parseString(doc, "timestamp");
+        if (timestamp == null || "".equals(timestamp)) {
+            timestamp = df.format(new Date());
+        }
+        ir.setTimeStamp(timestamp);
+        ir.setIpAddress(parseString(doc, "ip-address"));
+        ir.setReferer(parseString(doc, "referer"));
+        ir.setApplicationName(parseString(doc, "application-name"));
+        ir.setNameSpace(parseString(doc, "name-space"));
+        ir.setReportMethod(parseString(doc, "report-method"));
+        ir.setReportEmail(parseString(doc, "report-email"));
+        ir.setSendFeedback(parseBoolean(doc, "sendFeedback"));
+        ir.setUserId(parseString(doc, "userid"));
+        if (ir.isSendFeedback()) {
+            Element feedbackByMail = getElm(doc, "feedbackByMail");
+            ir.setFeedbackByMail(feedbackByMail.getAttribute("activated").equals("true"));
+            if (ir.isFeedbackByMail()) {
+                ir.setEmailAddress(parseString(feedbackByMail, "email"));
+            }
+
+            Element feedbackBySms = getElm(doc, "feedbackBySms");
+            ir.setFeedbackBySms(feedbackBySms.getAttribute("activated").equals("true"));
+            if (ir.isFeedbackBySms()) {
+                ir.setSmsPhoneNumber(parseString(feedbackBySms, "phoneNumber"));
+            }
+
+            Element feedbackByPhone = getElm(doc, "feedbackByPhone");
+            ir.setFeedbackByPhone(feedbackByPhone.getAttribute("activated").equals("true"));
+            if (ir.isFeedbackByPhone()) {
+                ir.setPhoneNumber(parseString(feedbackByPhone, "phoneNumber"));
+            }
+        }
+
+        ir.setSendScreenShot(parseBoolean(doc, "sendScreenShot"));
+        if (ir.isSendScreenShot()) {
+            NodeList files = doc.getElementsByTagName("file");
+            for (int i = 0; i < files.getLength(); i++) {
+                Node fileNode = files.item(i);
+                if (fileNode != null && fileNode.getFirstChild() != null) {
+                    String path = fileNode.getFirstChild().getTextContent().replaceFirst("file:", "").trim();
+                    path = path.replaceAll("%20", " ");
+                    File file = new File(path);
+                    Screenshot ss = new Screenshot();
+                    String fileName = "noname";
+                    try {
+                        fileName = fileNode.getAttributes().getNamedItem("filename").getTextContent();
+
+                    }
+                    catch (Exception e) {
+                        logger.warn("Filename could not be read.", e);
+                    }
+                    ss.setFileName(fileName);
+                    ss.setPath(file.getAbsolutePath());
+                    ir.addScreenShot(ss);
+
+                }
+            }
+        }
+
+        return ir;
+    }
+
+    private String parseString(Element parent, String elmName) {
+        if (parent.getElementsByTagName(elmName).getLength() == 0) {
+            return null;
+        }
+        Element elm = (Element) parent.getElementsByTagName(elmName).item(0);
+        return (elm.getFirstChild() == null ? null : elm.getFirstChild().getTextContent());
+
+    }
+
+    private String parseString(Document doc, String elmName) {
+        if (doc.getElementsByTagName(elmName).getLength() == 0) {
+            return "";
+        }
+        Element elm = (Element) doc.getElementsByTagName(elmName).item(0);
+
+        return (elm.getFirstChild() == null) ? "" : elm.getFirstChild().getTextContent();
+    }
+
+    private Element getElm(Document doc, String elmName) {
+        if (doc.getElementsByTagName(elmName).getLength() == 0) {
+            return null;
+        }
+        return (Element) doc.getElementsByTagName(elmName).item(0);
+
+    }
+
+    private boolean parseBoolean(Document doc, String elmName) {
+        if (doc.getElementsByTagName(elmName).getLength() == 0) {
+            return false;
+        }
+        Element elm = (Element) doc.getElementsByTagName(elmName).item(0);
+        return (elm.getFirstChild() == null) ? false : elm.getFirstChild().getTextContent().equals("true");
+    }
+
+    public boolean isReadable(Class<?> cls, Type arg1, Annotation[] arg2, javax.ws.rs.core.MediaType arg3) {
+        return IncidentReport.class.isAssignableFrom(cls);
+    }
+
+}

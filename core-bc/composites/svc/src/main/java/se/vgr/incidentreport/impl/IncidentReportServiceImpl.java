cd186a8afd0a7ae9ded497e0a8ad53a0910c7383
arkitekthatt <arkitekthatt@fe4735b6-2b13-11de-ba44-d58fe8a9ce33>
2009-11-12 10:41:55 +0000

    Preparations for 1.0 release

--- core-bc/composites/svc/src/main/java/se/vgr/incidentreport/impl/IncidentReportServiceImpl.java
+++ core-bc/composites/svc/src/main/java/se/vgr/incidentreport/impl/IncidentReportServiceImpl.java
@@ -148,8 +148,9 @@ public class IncidentReportServiceImpl implements IncidentReportService {
         if (url != null && attachments != null && attachments.size() > 0) {
             if (ir.getReportEmail() != null && ir.getReportEmail().length() > 0) {
                 try {
-                    ir.setDescription("PivotalTracker :" + url + " \n" + ir.getDescription());
-                    sendReportByEmail(ir, ir.getApplicationName() + ":Attachments for upload in pivotal tracker");
+                    ir.setLink("User story: <a href='" + url + "'>" + url + "</a></b>");
+
+                    sendReportByEmail(ir, "Bifogade filer som skall till pivotal tracker.");
                 }
                 catch (MessagingException e) {
 
@@ -165,7 +166,7 @@ public class IncidentReportServiceImpl implements IncidentReportService {
         if (reportEmail != null && !"".equals(reportEmail)) {
             String body = "";
             String[] reportEmailArray = { reportEmail };
-            body += ir.toString().replaceAll(IncidentReport.NEWLINE, "\n</br>");
+            body += ir.toString().replaceAll(IncidentReport.NEWLINE, "</br>");
             try {
                 new EMailClient().postMail(reportEmailArray, ir.getApplicationName() + ":" + subject, "" + body,
                         INCIDENT_REPORT_SERVICE_NOREPLY, ir.getScreenShots());
